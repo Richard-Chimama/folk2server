e8ffce32d47898af6b78b0801e1a14a6ebb82506
richard <rich.chime@gmail.com>
2024-07-15 01:04:58 +0200

    fixed the bug

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const HOST = "0.0.0.0";
 
-// const whitelist = ['http://0.0.0.0:4000', 'https://www.folktfolk.se']
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+const whitelist = ['http://0.0.0.0:4000', 'https://www.folktfolk.se']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 app.use(express.json());
 app.use(helmet())
-//app.use(CORS(corsOptions))
+app.use(CORS(corsOptions))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
@@ -64,14 +64,13 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {}
+  });
 });
+
 
 app.listen(PORT, HOST, () => {
   console.log(`server running on http://${HOST}:${PORT}`);
